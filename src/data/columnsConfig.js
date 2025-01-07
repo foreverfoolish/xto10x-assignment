2e40f97923a9e3ee3c3f962e6fe59a2f14a8a4ea
Manu <realmanusharma@gmail.com>
2025-01-07 18:41:31 +0530

    [feature-configurable-table]: initial commit

---
+++ src/data/columnsConfig.js
@@ -0,0 +1,10 @@
+const columns = [
+    { key: "id", label: "ID" },
+    { key: "name", label: "Name" },
+    { key: "date", label: "Date" },
+    { key: "status", label: "Status" },
+    { key: "amount", label: "Amount" },
+  ];
+  
+  export default columns;
+  
